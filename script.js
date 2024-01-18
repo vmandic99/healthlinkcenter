27242d7aa5a5e123116c39d7ffb3a28f0626b983
vikmandic99 <viktor_mandic@outlook.com>
2024-01-18 13:57:09 +0100

    Deine Commit-Nachricht hier

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+async function checkUserLoggedIn() {
+  const userUid = localStorage.getItem("useruid");
+  const userEmail = localStorage.getItem("useremail");
+
+  if (userUid === null || userEmail === "null") {
+    alert("Bitte logge dich ein!");
+    window.location.href = "index.html";
+  } else {
+    // Hier kannst du weiteren Code ausführen, der nach dem Einloggen benötigt wird
+    console.log("Benutzer eingeloggt:", userUid, userEmail);
+  }
+}
+
+// Aufruf der asynchronen Funktion
+checkUserLoggedIn();
+
+
 // Funktion zur Verschlüsselung einer E-Mail-Adresse
 function encryptEmail(email) {
     var encryptedString = btoa(email);
